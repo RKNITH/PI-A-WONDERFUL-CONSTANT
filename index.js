0472910dea24b165a799f481c99a01accecec3a5
RKNITH <raviranjankofficial@gmail.com>
2024-08-08 22:25:14 +0530

    file changed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // scripts.js
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+    const closeBtn = document.getElementById('close-btn');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('nav-active');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        navMenu.classList.remove('nav-active');
+    });
+});
